7cd382578172f5db1cb8480a5c67fab3b006c4af
TanushreeMohanty <122718553+TanushreeMohanty@users.noreply.github.com>
2023-07-21 22:59:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -47,31 +47,31 @@ function initMap() {
 
 /*Project Animation*/
 
-  const projectCards = document.querySelectorAll('.project-card');
+const projectCards = document.querySelectorAll('.project-card');
 
-  function isInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
 
-  function handleScrollAnimation() {
-    projectCards.forEach((card) => {
-      if (isInViewport(card)) {
-        card.classList.add('appear');
-      } else {
-        card.classList.remove('appear');
-      }
-    });
-  }
+function handleScrollAnimation() {
+  projectCards.forEach((card) => {
+    if (isInViewport(card)) {
+      card.classList.add('appear');
+    } else {
+      card.classList.remove('appear');
+    }
+  });
+}
 
-  // Call the function on initial load and on scroll
-  handleScrollAnimation();
-  window.addEventListener('scroll', handleScrollAnimation);
+// Call the function on initial load and on scroll
+handleScrollAnimation();
+window.addEventListener('scroll', handleScrollAnimation);
 
 /*SERVICES*/
 const canvas = document.getElementById('canvas');
@@ -139,4 +139,12 @@ function scrollDirection() {
 window.onload = animateServiceCards;
 window.addEventListener('scroll', scrollDirection);
 
-
+// Form validation
+const contactForm = document.getElementById('contactForm');
+contactForm.addEventListener('submit', function(event) {
+  if (!contactForm.checkValidity()) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+  contactForm.classList.add('was-validated');
+});
